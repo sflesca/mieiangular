9e2ee673266d5d65914561f923754cae5f0ddeb5
sflesca <sergio.flesca@unical.it>
2024-05-01 17:39:30 +0200

    aggiunti eventi

--- psw2024/src/app/cicciobello/cicciobello.component.ts
+++ psw2024/src/app/cicciobello/cicciobello.component.ts
@@ -9,4 +9,8 @@ import { Component, Input } from '@angular/core';
 })
 export class CicciobelloComponent {
   @Input() nome!:string
+
+  cambia(){
+    this.nome = this.nome + ' ' + this.nome
+  }
 }
