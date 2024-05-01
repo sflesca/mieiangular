fa8121e6b1255740230311ddd68e0486d6907490
sflesca <sergio.flesca@unical.it>
2024-05-01 17:08:16 +0200

    aggiornato standalone

--- psw2024/src/app/cicciobello/cicciobello.component.ts
+++ psw2024/src/app/cicciobello/cicciobello.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-cicciobello',
@@ -8,5 +8,5 @@ import { Component } from '@angular/core';
   styleUrl: './cicciobello.component.css'
 })
 export class CicciobelloComponent {
-
+  @Input() nome!:string
 }
